2630ef349b1e7d5113ca7408e9beb1c8c10ce58a
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-02-07 14:06:33 +0530

    Add: Backend Server

---
+++ backend/src/index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+
+// Dummy Data
+const events = [
+    {
+        id: "hackathon",
+        name: "Esperanza Hackathon",
+        description: `Esperanza Hackathon is a coding competition organized by your college aimed 
+        at encouraging students to develop innovative technology solutions to real-world problems. 
+        The event provides a platform for students to showcase their coding skills, collaborate with 
+        peers, and gain hands-on experience in software development. Participants will have the 
+        opportunity to work on exciting projects and receive mentorship from experienced industry 
+        professionals. The hackathon will culminate in a final presentation where teams will present 
+        their solutions to a panel of judges. With prizes, recognition, and opportunities for networking 
+        and career development, Esperanza Hackathon is a must-attend event for all aspiring tech enthusiasts.`,
+    },
+    {
+        id: "chess",
+        name: "Esperanza Chess Battle",
+        description: `Esperanza Chess Competition is a strategic and intellectual event organized by your college 
+        for students to showcase their chess skills and compete against peers. This competition is open to students 
+        of all skill levels, from beginners to experienced players. The event aims to provide an exciting platform 
+        for participants to improve their chess strategies, learn new techniques, and make new friends. The 
+        competition will feature a series of rounds where participants will face off against each other in a game 
+        of chess. The winner will be determined based on their performance throughout the tournament and will be 
+        awarded a prize. Esperanza Chess Competition is a great opportunity for students to challenge themselves 
+        and showcase their skills to a wider audience. Whether you're a seasoned chess player or just starting out, 
+        this competition is the perfect place to showcase your skills and have a great time!`,
+    },
+];
+
+// Default Port
+const PORT = 8081;
+const app = express();
+
+// Using JSON Middleware
+app.use(express.json());
+
+// Accepting GET Request
+app.get("/event/:id", async (req, res) => {
+    const { id } = req.params;
+
+    events.map((value, index) => {
+        if (value.id === id) {
+            res.json(value);
+        }
+    });
+
+    res.status(404).send("No matching event found!");
+});
+
+// Initializing Express at default Port
+app.listen(PORT, () => {
+    console.log(`Backend server is running on port ${PORT}`);
+});
